eb3ac85a848010f74b1aa3733b5187fe3e16cb38
jarlaxle-536 <eugenegarusov@gmail.com>
2020-10-19 14:22:17 +0300

    + settings serializer fields

--- chatterboxes/chatterboxes/applications/main/serializers.py
+++ chatterboxes/chatterboxes/applications/main/serializers.py
@@ -1,4 +1,20 @@
 from rest_framework import serializers
 
 class ChatSettingsSerializer(serializers.Serializer):
-   allow_bots = serializers.BooleanField()
+    client_gender = serializers.ChoiceField(
+        choices=['male', 'female'],
+        style={'base_template': 'radio.html'}
+    )
+    client_age = serializers.ChoiceField(
+        choices=['0-15', '15-25', '25-55', '55-100'],
+        style={'base_template': 'radio.html'}
+    )
+    interlocutor_gender = serializers.ChoiceField(
+        choices=['male', 'female'],
+        style={'base_template': 'radio.html'}
+    )
+    interlocutor_age = serializers.ChoiceField(
+        choices=['0-15', '15-25', '25-55', '55-100'],
+        style={'base_template': 'radio.html'}
+    )
+    allow_bots = serializers.BooleanField()
--- chatterboxes/chatterboxes/applications/main/views.py
+++ chatterboxes/chatterboxes/applications/main/views.py
@@ -14,8 +14,22 @@ class SettingsAPI(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'chat_settings.html'
     def get(self, request):
-        data = {'allow_bots': False}
+        data = {
+            'allow_bots': False,
+            'client_gender': 'male',
+            'client_age': '0-15',
+            'interlocutor_gender': 'male',
+            'interlocutor_age': '0-15',
+        }
         context = {
             'chat_settings_form': ChatSettingsSerializer(data),
         }
         return Response(context)
+    def post(self, request):
+        pass
+
+class ChatAPI(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'chat.html'
+    def get(self, request):
+        return Response()
